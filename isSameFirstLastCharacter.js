0734b020f2443d1e066c22bd62f359ef99d90c7d
wnf <william@fluxrme.com>
2023-10-03 18:29:04 -0500

    exercises complete

--- isSameFirstLastCharacter.js
+++ isSameFirstLastCharacter.js
@@ -2,6 +2,10 @@ function isSameFirstLastCharacter(str) {
   return str.charAt(0) === str.charAt(str.length - 1);
 }
 
+console.log(isSameFirstLastCharacter('a'))
+console.log(isSameFirstLastCharacter('aa'))
+console.log(isSameFirstLastCharacter('ab'))
+
 console.log(isSameFirstLastCharacter(""));
 
 module.exports = isSameFirstLastCharacter;
--- isSameFirstLastCharacter.test.js
+++ isSameFirstLastCharacter.test.js
@@ -12,17 +12,27 @@ test("it should return true when the first and last character are the same", () 
 /* A test case for a string that does NOT have the same first and last 
    character and is 4+ characters long
 */
-test("", () => {});
+test("it should return false when the first and last character are different", () => {
+   expect(isSameFirstLastCharacter("abbbbbb")).toBe(false);
+});
 
 // Edge Cases
 // A test case for a string with only 2 characters that are different
-test("", () => {});
+test("it should return false when both characters in a two-character string are different", () => {
+   expect(isSameFirstLastCharacter("xy")).toBe(false);
+});
 
 // A test case for a string with only 2 characters that are the same
-test("", () => {});
+test("it should return true when both characters in a two-character string are the same", () => {
+   expect(isSameFirstLastCharacter("aa")).toBe(true);
+ });
 
 // A test case for a string with only 1 character
-test("", () => {});
+test("it should return true when the string is only one character", () => {
+   expect(isSameFirstLastCharacter("x")).toBe(true)
+});
 
 // A test case for an empty string
-test("", () => {});
+test("it should return false when the input is an empty string", () => {
+   expect(isSameFirstLastCharacter('')).toBe(false)
+});
